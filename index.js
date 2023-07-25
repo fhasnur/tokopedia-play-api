3eedd74f229c89e546c7f00850705d7673500b98
fhasnur <fandi.hasnur10@gmail.com>
2023-07-25 21:42:35 +0800

    feat: add configuration for connect to database

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+/* eslint-disable no-undef */
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3000;
+dotenv.config();
 
-app.use(express.json());
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.log(err);
+});
+
+db.once('connected', () => {
+  console.log('Database connected');
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
