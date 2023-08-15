a390c9fc9e0f21cfb54992fbaff842e4bba0f5f1
fhasnur <fandi.hasnur10@gmail.com>
2023-08-15 09:44:09 +0800

    chore: set headers and modify routes

--- index.js
+++ index.js
@@ -33,9 +33,15 @@ app.use(
   }),
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/videos', videos);
-app.use('/product', products);
-app.use('/comment', comments);
+app.use('/products', products);
+app.use('/comments', comments);
 
 app.use((req, res) => {
   res.status(404).json({
