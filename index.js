9521bd4f03c38b7adc38146699bdad58cef67e0a
fhasnur <fandi.hasnur10@gmail.com>
2024-07-11 18:47:58 +0700

    refactor: mongodb connection error handling

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 
 import videos from './routes/videos.js';
 import products from './routes/products.js';
@@ -11,27 +10,24 @@ import comments from './routes/comments.js';
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 
-mongoose.connect(DB_URL);
-const db = mongoose.connection;
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to database');
 
-db.on('error', (error) => console.error(error));
-db.once('open', async () => {
-  console.log('Connected to database');
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error('Could not connect to database:', error));
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-});
-
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
